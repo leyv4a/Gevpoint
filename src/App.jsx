efa29372630be69f04daf510dd64bde85abfe268
leyv4a <gabriel.leyva228902@potros.itson.edu.mx>
2024-02-20 15:04:05 -0700

    Table 'productos' fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Sidebar from './components/shared/Sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Routes} from "react-router-dom";
-import Productos from "./components/pages/productos";
+import Productos from "./components/pages/Productos";
 import Dashboard from "./components/pages/Dashboard";
 import ProductosEntradas from "./components/pages/ProductosEntradas";
 import Ventas from "./components/pages/Ventas";
@@ -12,10 +12,10 @@ function App() {
   return (
      <div className='container-fluid bg-dark-subtle '>
      <div className='row'> 
-        <div className='col-auto bg-dark col-md-1 order-first min-vh-100 border-end border-3 border-light' style={{"--bs-border-opacity": ".5"}}>
+        <div className='col-auto bg-dark col-md-1 order-first border-end border-3 border-light' style={{"--bs-border-opacity": ".5", height: '100vh'}}>
             <Sidebar/>
         </div>
-        <div className='col-auto col-md-11 order-last'>
+        <div className='col-auto col-md-11 order-last ' style={{overflowY: 'hidden'}}   >
           <Routes>
             <Route path="/productos" element={<Productos/>}/>
             <Route path="/dashboard" element={<Dashboard/>}/>
--- src/components/pages/Productos.jsx
+++ src/components/pages/Productos.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HeaderInventario from '../shared/headerInventario';
 import { FaWarehouse } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
 import { MdMiscellaneousServices } from "react-icons/md";
+import Axios from 'axios';
 
 
 function Productos() {
+
+  const [productos, setProductos] = useState([]);
+
+  const getProductos = (val) => {
+    Axios.get('http://localhost:3000/items')
+   .then(response => {
+        setProductos(response.data);
+        console.log("Data readed successfully"+ response.data);
+      })
+   .catch(error => {
+        console.log(error);
+      });
+  }
+
+  useEffect(()=>{
+    getProductos();
+  },[]);
+
   return (
-    <main className='m-3' >
+    <main className='m-3'>
      <HeaderInventario isInventario={true}><FaWarehouse/>Inventario</HeaderInventario>
       <hr/>
-      <div className='mt-2 bg-light  overflow-y-scroll'>
-        <table className="table table-striped">
+      <div className='mt-2 bg-light' style={{overflowY: 'scroll', maxHeight: '75vh'}}>
+        <table className="table table-striped table-hover">
          <thead className='sticky-top'>
             <tr>
-                <th scope="col">#</th>
                 <th scope="col">Nombre</th>
                 <th scope="col">Codigo</th>
                 <th scope="col">Impuesto</th>
@@ -24,37 +42,22 @@ function Productos() {
                 <th scope="col" className='fs-4'><MdMiscellaneousServices/></th>
             </tr>
          </thead>
-         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Manzana</td>
-                <td>F1</td>
-                <td>N/A</td>
-                <td>49.90</td>
-                <td>Frutas y verduras</td>
-                <td>54kg</td>
-                <td><button className='btn btn-outline-warning'><MdEdit/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Pera</td>
-                <td>F2</td>
-                <td>N/A</td>
-                <td>41.90</td>
-                <td>Frutas y verduras</td>
-                <td>30kg</td>
-                <td><button className='btn btn-outline-warning'><MdEdit/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Salsa Huichol</td>
-                <td>A1</td>
-                <td>N/A</td>
-                <td>19.00</td>
-                <td>Abarrotes</td>
-                <td>22</td>
-                <td><button className='btn btn-outline-warning'><MdEdit/></button></td>
-            </tr>
+         <tbody >
+           {
+             productos.map(producto => {
+               return (
+                 <tr key={producto.id}>
+                   <td>{producto.nombre}</td>
+                   <td>{producto.codigo}</td>
+                   <td>{producto.impuesto}</td>
+                   <td>{producto.precio}</td>
+                   <td>{producto.Categoria}</td>
+                   <td>{producto.cantidad}</td>
+                   <td><button className='btn btn-outline-warning'><MdEdit/></button></td>
+                 </tr>
+               )
+             })
+           }
          </tbody>
         </table>
     </div>    
